ac5234c5f350c36cba06f2c73c46f2cb5b874823
Gabrielb-del <gabrielbaunilia123@gmail.com>
2024-10-21 17:24:26 -0300

    não sei

--- b4bot.py
+++ b4bot.py
@@ -14,12 +14,6 @@ def carregar_planilha(caminho_planilha):
         df['Status'] = ''  # Inicializa a coluna 'Status' se não existir
     return df
 
-def dividir_nome_completo(nome_completo):
-    partes = nome_completo.split()
-    nome = partes[0]  # Primeiro nome
-    sobrenome = " ".join(partes[1:]) if len(partes) > 1 else ""  # O resto é o sobrenome
-    return nome, sobrenome
-
 
 def iniciar_driver():
     options = webdriver.ChromeOptions()
@@ -30,27 +24,8 @@ def iniciar_driver():
     return driver
 
 # Função para verificar se o cliente pode ser cadastrado
-def verificar_cliente(driver, nome, sobrenome, email, telefone, cnpj):
+def verificar_cliente(driver, cnpj):
     try:
-        print("Preenchendo o nome...")
-        nome_input = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//input[contains(@id, '610:0')]")))
-        nome_input.clear()
-        nome_input.send_keys(nome)
-
-        print("Preenchendo o sobrenome...")
-        sobrenome_input = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//input[contains(@id, '620:0')]")))
-        sobrenome_input.clear()
-        sobrenome_input.send_keys(sobrenome)
-
-        print("Preenchendo o email...")
-        email_input = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//input[contains(@id, '655:0')]")))
-        email_input.clear()
-        email_input.send_keys(email)
-
-        print("Preenchendo o telefone...")
-        telefone_input = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//input[contains(@id, '671:0')]")))
-        telefone_input.clear()
-        telefone_input.send_keys(telefone)
 
         print("Preenchendo o CNPJ...")
         cnpj_input = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//input[contains(@id, '708:0')]")))
@@ -58,10 +33,10 @@ def verificar_cliente(driver, nome, sobrenome, email, telefone, cnpj):
         cnpj_input.send_keys(cnpj)
 
         # Submeter o formulário
-        submit_button = driver.find_element(By.XPATH, "//button[contains(@class, 'slds-button') and contains(@class, 'uiButton--brand') and span[text()='Confirmar']]")
+        submit_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'slds-button') and contains(@class, 'uiButton--brand') and span[text()='Confirmar']]")))
         submit_button.click()
         
-        time.sleep(1)
+        time.sleep(2)
 
         # Verificar se o erro "Telefone precisa começar com 55" aparece
         try:
@@ -107,18 +82,10 @@ def processar_verificacao(caminho_planilha):
     driver = iniciar_driver()
 
     for index, row in df.iterrows():
-        # Dividir nome completo
-        nome_completo = row['NOME']
-        nome, sobrenome = dividir_nome_completo(nome_completo)
-        
-        email = row['EMAIL']
-        
-        # Adicionar DDI ao telefone
-        telefone = row['TELEFONE']
         
         cnpj = row['CNPJ']
 
-        status = verificar_cliente(driver, nome, sobrenome, email, telefone, cnpj)
+        status = verificar_cliente(driver, cnpj)
         df.at[index, 'Status'] = status  # Atualizar a coluna "Status" com o resultado
 
 
